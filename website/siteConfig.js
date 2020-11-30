c4855dc06614d41ccfcc8471cd749361ca5134f0
gonzalojuarez <gonjuarez@ottaaproject.com>
2020-11-30 16:09:15 -0300

    no message

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'OTTAA Project', // Title for your website.
   tagline: '',
-  url: 'https://github.com/orgs/OTTAA-Project', // Your website URL
-  baseUrl: '/master/', // Base URL for your project */
+  url: 'https://ottaa-project.github.io/ottaaproject.github.io/', // Your website URL
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
