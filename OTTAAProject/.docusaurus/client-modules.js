8e6c8fbcb5d00dc22070ef62bcb829382e128752
gonojuarez <gonojuarez17@gmail.com>
2021-02-10 12:56:10 -0300

    commit
    circleci

--- OTTAAProject/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/ottaaproject/Documents/GitHub/documentation/OTTAAProject/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ottaaproject/Documents/GitHub/documentation/OTTAAProject/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ottaaproject/Documents/GitHub/documentation/OTTAAProject/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/ottaaproject/Documents/GitHub/documentation/OTTAAProject/node_modules/bootstrap/dist/css/bootstrap.min.css"),
-];
